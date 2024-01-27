e07a90e3fae0275305bd24a6e49fddceaebf4ac1
OlineRanum <oline.a.ranum@gmail.com>
2024-01-27 19:32:12 +0100

    Restructured codebase

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from config.configure_utils import SetUp
-from utils.hotkey_controler import Controller
+from src.config.configure_utils import SetUp
+from src.hotkey_controler import Controller
 
 if __name__ == "__main__":
     args = SetUp("config/config.yaml")
-    
+
     key_controller = Controller(args)
     key_controller.start()
