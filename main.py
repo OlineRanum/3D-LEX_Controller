7241a269cf01271f55cf1bcfcee2e8c8353fcb95
OlineRanum <oline.a.ranum@gmail.com>
2024-01-27 19:16:31 +0100

    changed name to controller

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from config.configure_utils import SetUp
-from utils.hotkey_controler import KeyController
+from utils.hotkey_controler import Controller
 
 if __name__ == "__main__":
     args = SetUp("config/config.yaml")
-    key_controller = KeyController(args)
+    
+    key_controller = Controller(args)
     key_controller.start()
