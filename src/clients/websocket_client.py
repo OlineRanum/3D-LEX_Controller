6f59da0199364aa44fb313556de51fe03ca86c92
OlineRanum <oline.a.ranum@gmail.com>
2024-01-29 16:44:25 +0100

    websocket template

--- src/clients/websocket_client.py
+++ src/clients/websocket_client.py
@@ -2,28 +2,16 @@ import asyncio
 import websockets
 
 class WebSocketClient:
-    def __init__(self, uri):
-        self.uri = uri
+    def __init__(self):
 
-    async def connect(self):
-        async with websockets.connect(self.uri) as websocket:
-            # Send a request for data (if needed)
-            # await websocket.send("your request")
+        # Get uri from sys 
+        self.uri = ...
 
-            # Wait for a response and receive data
-            data = await websocket.recv()
-            return data
-    
+    def connect(self):
+        raise NotImplementedError
 
     def retrieve_next_gloss(self):
         raise NotImplementedError
 
 if __name__ == "__main__":
-    async def main():
-        client = WebSocketClient("https://www.gomerotterspeer.nl/Gebarenoverleg/lijst.html")
-        data = await client.connect()
-        print(data)
-
-    # Run the asyncio event loop
-    asyncio.run(main())
-
+    raise NotImplementedError
