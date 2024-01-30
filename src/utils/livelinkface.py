a6abe2418f5b88ede8a842235edb3b3d91608921
OlineRanum <oline.a.ranum@gmail.com>
2024-01-30 11:12:04 +0100

    added live link face client setup

--- src/utils/livelinkface.py
+++ src/utils/livelinkface.py
@@ -1,1 +1,25 @@
 from pythonosc import osc_server
+from pythonosc.dispatcher import Dispatcher
+
+
+class LiveLinkFaceClient:
+    def __init__(self, args):
+        self.dispatcher = Dispatcher()
+        self.dispatcher.map("/filter", print)
+        self.dispatcher.map("/OSCSetSendTargetConfirm", print)
+        self.dispatcher.map("/volume", self.print_volume_handler, "Volume")
+
+        self.server = osc_server.ThreadingOSCUDPServer(
+            (args.llf_ip, args.llf_port), self.dispatcher)
+
+        print("Serving on {}".format(self.server.server_address))
+        self.server.server_forever()
+
+    def print_volume_handler(self, unused_addr, args, volume):
+        print("[{0}] ~ {1}".format(args[0], volume))
+    
+    def print_compute_handler(self, unused_addr, args, volume):
+        try:
+            print("[{0}] ~ {1}".format(args[0], args[1](volume)))
+        except ValueError:
+            pass
