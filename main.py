9b0c8b2540aafdd30fff0ddedcbe406f51045fb0
OlineRanum <oline.a.ranum@gmail.com>
2024-01-30 13:45:14 +0100

    fix path bug

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.config.setup import SetUp
 from src.controler import Controller
 
 if __name__ == "__main__":
-    args = SetUp("config/config.yaml")
+    args = SetUp("config.yaml")
 
     key_controller = Controller(args)
     key_controller.start()
