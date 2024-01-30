8445ab8402d015812ca3c296110c5c0c610a28c8
OlineRanum <oline.a.ranum@gmail.com>
2024-01-30 11:17:57 +0100

    Setting up livelinkface server

--- src/utils/livelinkface.py
+++ src/utils/livelinkface.py
@@ -1,8 +1,23 @@
+from pythonosc.udp_client import SimpleUDPClient
 from pythonosc import osc_server
 from pythonosc.dispatcher import Dispatcher
 
 
 class LiveLinkFaceClient:
+    def __init__(self, args):
+        self.client = SimpleUDPClient(args.llf_udp_ip, args.llf_udp_port)
+        self.client.send_message("/OSCSetSendTarget", [args.llf_ip, args.llf_port])
+        self.client.send_message("/VideoDisplayOff", [])
+    
+    def start_capture(self):
+        self.client.send_message("/RecordStart", ["Calibratie", 3])
+    
+    def stop_capture(self):
+        self.client.send_message("/RecordStop", [])
+
+
+
+class LiveLinkFaceServer:
     def __init__(self, args):
         self.dispatcher = Dispatcher()
         self.dispatcher.map("/filter", print)
