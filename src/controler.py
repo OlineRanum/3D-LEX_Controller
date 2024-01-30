eeb926332a33f1f5f185c71a669e489f2cfdfde6
OlineRanum <oline.a.ranum@gmail.com>
2024-01-30 13:45:07 +0100

    commented out websocket for testing

--- src/controler.py
+++ src/controler.py
@@ -1,7 +1,8 @@
 from pynput import keyboard
-from utils.vicon import ShogunClient
-from utils.websocket_client import WebSocketClient
-from utils.filemanager import FileManager
+from src.utils.vicon import ShogunClient
+from src.utils.websocket_client import WebSocketClient
+from src.utils.filemanager import FileManager
+from src.utils.livelinkface import LiveLinkFaceClient, LiveLinkFaceServer
 
 
 class Controller:
@@ -15,22 +16,31 @@ class Controller:
         self.args = args
 
         # For managing and cleaning up files
-        self.file_manager = FileManager()
+        self.file_manager = FileManager(self.args.output_dir)
 
         # TODO: Set up websocket client for Gomer
         # Interactions with Gebarenoverleg platform
         #self.wsc  = WebSocketClient()
 
+        # Initialize first sign
         # Issue request to ws to display first gloss
         #self.gloss = self.wsc.retrieve_next_gloss()
+        # TODO: build directory
 
         # TODO: Remove
         # For testing 
         self.gloss = "testgloss"
+        self.file_manager.create_directory(self.args.output_dir + '/' + self.gloss, subdir = True)
 
         # Interactions with Vicon Shogun
         self.vc = ShogunClient(self.args, self.gloss)
 
+        # Interactions with HandEngine
+        # Currently controlled through integrations in shogun live
+
+        # Interactions with Live Link Face / iPhone AR Kit
+        self.llfc = LiveLinkFaceClient(self.args, self.gloss) 
+        self.llfs = LiveLinkFaceServer(self.args) 
         
         # File Counter  
         # We do it like this to not have to search through all files in a directory 
@@ -39,42 +49,72 @@ class Controller:
 
 
 
-        
-
     def __on_press(self, key):
         try:
+            #--------------------------------------------
+            # Start recording when start key is pressed
+            #--------------------------------------------
             if key.char == self.args.start_key:
                 # Remove old version if it exists
                 if self.file_counter == 1:
-                    self.file_manager.remove_file(self.gloss)
-
-                print('{0}: start recording'.format(key.char))
-                #emit signal to vicon client to start recording
-                self.vc.start_capture()
-
-            elif key.char == self.args.stop_key:
-                print('{0}: stop recording'.format(key.char))
-                # emit signal to vicon client to stop recording
-                self.vc.stop_capture()
+                    # TODO: build function to clean directory
+                    self.file_manager.clean_dir(self.args.output_dir + '/' + self.gloss)
                 
+                print('{0}: start recording'.format(key.char))
+                # Trigger vicon to start recording
+                self.vc.start_capture()
+                # Hand engine automatically triggered if trigger congifured in shogun live
+
+                # Trigger LLF to start recording
+                self.llfc.start_capture()
+
                 self.file_counter = 1
 
+            #--------------------------------------------
+            # Stop recording when stop key is pressed
+            #--------------------------------------------
+            elif key.char == self.args.stop_key:
+                print('{0}: stop recording'.format(key.char))
+                # Trigger vicon to stop recording
+                # Hand engine automatically triggered if trigger congifured in shogun live
+                self.vc.stop_capture()
+
+                # Trigger LLF to stop recording
+                self.llfc.stop_capture()
+                
+            #--------------------------------------------
+            # Save recording when save key is pressed
+            #--------------------------------------------
             elif key.char == self.args.save_key:
                 print('{0}: save recording'.format(key.char))
+                # Save recording in vicon
                 self.vc.save_capture(self.gloss)
                 
-                # Request next gloss from 
-                self.wsc.request_next_gloss()
+                # Export recording from LLF to computer 
+                self.llfs.export_capture(self.gloss)
+
+                # collect an pickle data
+                self.file_manager.export_pickle(self.gloss)
+
+                # Request next gloss from Gebarenoverleg platform
+                #self.gloss = self.wsc.request_next_gloss()
+
+                # TODO: build directory
+                self.file_manager.create_directory(self.args.output_dir + '/' + self.gloss)
+                
                 
                 # Set the filename to the next gloss
                 self.vc.set_filename(self.gloss)
+                self.llfc.set_filename(self.gloss)
 
                 # reset file_counter
                 self.file_counter = 0
+            else: 
+                print('Key not mapped to functionality, please configure in config.yaml')
 
-        except AttributeError:
+        except AttributeError as e:
             # Handles special keys that don't have a char attribute
-            print('Key not mapped to functionality, please configure in config.yaml')
+            print(f"AttributeError: {e}")
 
 
     def __on_release(self, key):
