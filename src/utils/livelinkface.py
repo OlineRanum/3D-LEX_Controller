a05cee783ab1936db84891c711fdd7bf5c9af0f1
AndersenJari99 <AndersenJari99@gmail.com>
2024-02-01 10:00:15 +0100

    Commented and cleaned the livelinkface python script a bit

--- src/utils/livelinkface.py
+++ src/utils/livelinkface.py
@@ -6,7 +6,11 @@ import asyncio
 from asyncio import run 
 import os 
 
+# Class LiveLinkFaceClient sends messages to the live link server on the IPhone
+# Sometimes it will be requested to save the file by the python server, and other times
+# it will be requested to send messages by the IO
 class LiveLinkFaceClient:
+    # The init starts the client and sets the python server address on the IPhone
     def __init__(self, args, gloss):
         self.client = SimpleUDPClient(args.llf_udp_ip, args.llf_udp_port)
         self.client.send_message("/OSCSetSendTarget", [args.target_ip, args.target_port])
@@ -21,7 +25,7 @@ class LiveLinkFaceClient:
     def start_capture(self):
         self.client.send_message("/RecordStart", [self.gloss, self.takenumber])
         return self.takenumber
-    
+
     def stop_capture(self):
         self.client.send_message("/RecordStop", [])
         self.takenumber += 1
@@ -36,48 +40,47 @@ class LiveLinkFaceClient:
         print('request battery')
         self.client.send_message("/BatteryQuery", [])
 
-
     def save_file(self, *args):
         print(f"{args}")
         self.client.send_message("/Transport", [self.args.target_ip + ':' + str(self.args.target_port), os.getcwd() + '\output'])
         print(os.getcwd() + '\output\\' + self.gloss)
         print(self.args.target_ip + ':' + str(self.args.target_port))
-    
-    def default(self, address, *args):
-        print(f"{address}: {args}")
 
+# Class LiveLinkFaceServer launches the live link server that communicates with the IPhone
+# The IP used in this server should be the same as the listener in the IPhone
+# The server is launched asynchronously and the main loop will continue afterwards
+# Because the server also needs to confirm some messages of the IPhone, it uses some client functions from
+# the LiveLinkFaceClient Class
 class LiveLinkFaceServer:
     def __init__(self, controller):
         self.gloss = controller.gloss
         self.args = controller.args
+      
+        # Start server rules here, add a default rule for all other incoming messages
         self.dispatcher = Dispatcher()
-        
-        self.dispatcher.map("/RecordStop", controller.llfc.stop_capture)
         self.dispatcher.map("/OSCSetSendTargetConfirm", print)
-        self.dispatcher.map("/volume", self.print_volume_handler, "Volume")
-        #self.dispatcher.map("/RecordStopConfirm", controller.llfc.save_file, "fsaf")
-        self.dispatcher.set_default_handler(controller.llfc.default)
+
+        # When the recording is fully finished, instruct the client to save the file locally
+        self.dispatcher.map("/RecordStopConfirm", controller.llfc.save_file, "fsaf")
+        self.dispatcher.set_default_handler(self.default)
+
+        # Launch Server
         run(self.init_server(controller))
         
 
+    # Server async launch code and close code, serving on target ip and port
     async def init_server(self, controller):
         self.server = osc_server.AsyncIOOSCUDPServer(
             (self.args.target_ip, self.args.target_port), self.dispatcher, asyncio.get_event_loop())
         transport, protocol = await self.server.create_serve_endpoint()
 
-        # enter main of program
+        # Enter main program async
         await controller.start()
         print('we are here')
 
+        # After we are done with the server, close it
         transport.close()
         
-
-    def print_volume_handler(self, unused_addr, args, volume):
-        print("[{0}] ~ {1}".format(args[0], volume))
-    
-    def print_compute_handler(self, unused_addr, args, volume):
-        try:
-            print("[{0}] ~ {1}".format(args[0], args[1](volume)))
-        except ValueError:
-            pass
-    
+    # Print all messages by default
+    def default(self, address, *args):
+        print(f"{address}: {args}")
